8a5d31d95889cfbe326274bce1cec25dc63f4922
Mondher <mondhermsp@gmail.com>
2025-02-18 11:37:49 +0100

    Removed 'cancel' status and added 'archived' attribute instead

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,40 +1,45 @@
 const mongoose = require("mongoose");
 
-const TaskSchema = new mongoose.Schema({
-  title: { 
-    type: String, 
-    required: true 
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["TO_DO", "DOING", "DONE"],
+      default: "TO_DO",
+    },
+    dateDebut: {
+      type: Date,
+      required: false,
+    },
+    dateTerminee: {
+      type: Date,
+      required: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    doneBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+    archived: {
+      type: Boolean,
+      default: false,
+    },
   },
-  description: { 
-    type: String, 
-    required: true,
- },
- status: { 
-    type: String, 
-    enum: ["à faire", "en cours", "terminé", "annulé"], 
-    default: "à faire" 
-  },
-  dateDebut: { 
-    type: Date, 
-    required:false
- },
-  dateTerminee: { 
-    type: Date, 
-    required:false
-
- },
-  createdBy: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: "User", 
-    required: true 
-},
-doneBy: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: "User", 
-    required: false 
-},
-}, 
-{ timestamps: true }); // Active `createdAt` et `updatedAt` automatiquement
+  { timestamps: true }
+); // Active `createdAt` et `updatedAt` automatiquement
 
 // Middleware pour mettre à jour `updatedAt` à chaque modification
 TaskSchema.pre("save", function (next) {
